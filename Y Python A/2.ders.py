aa3684bcc378629f48b265cd30345b0d40afb730
Skytech <93709385+saitTheCoder@users.noreply.github.com>
2023-08-08 21:15:40 +0300

    Add files via upload

---
+++ Y Python A/2.ders.py
@@ -0,0 +1,29 @@
+# kelime = "kitap"
+# kelime = "kalem"
+# kelime = "klavye"
+
+# print(kelime)
+
+# sepet = "muz"
+# print(sepet)
+# sepet = sepet + "elma"
+# print(sepet) #muzelma
+# sepet = sepet + "armut"
+# print(sepet)
+# sepet = sepet + "portakal"
+# print(sepet)
+
+
+age = 20
+name = "ahmet"
+sayi = 10.5
+
+print(name + " is " + str(age) + " years old") # "20"
+# veri tipini ogrenme --> type()
+# tip donusturme
+# str() ---> stringe donusturur
+# int() ---> tam sayiya donusturur
+# float() --> ondalikli sayiya donusturur
+print(type(name))
+print(type(age))
+print(type(sayi))
---
+++ Y Python A/first.py
@@ -0,0 +1,35 @@
+# Temel Veri Tipleri
+# 1. String - "@3g*)" '10' Text
+# 2. Sayilar
+# Tam sayi 100
+# Ondalikli sayi 39.99
+# yuzde 10 zam geldi
+# print(10.99 * 1.1)
+# print(10.99 / 1.1)
+# print(10.99 + 1.1)
+# print(10.99 - 1.1)
+
+# String Birlestirme (+)
+# print("sait"+"ibrahim") #"saitibrahim"
+# print("sait","ibrahim",10,29)
+
+# Variables - Degiskenler
+name = "Ahmet"
+surname = "Karatas"
+# mesaj = "Adi "+name+" soyadi "+surname+" olan kullanici sisteme giris yapti"
+# print(mesaj)
+# print("Hosgeldin "+name+"!!!")
+benim_arabam_degil = 10
+# print(help("keywords"))
+# Case Sensitive - buyuk kucuk harf duyarli
+
+# Input Fonksiyonu
+print(1)
+print(2)
+isim = input("Adinizi giriniz: ")
+soyisim = input("Soyisminizi giriniz: ")
+mesaj = "Adi "+isim+" soyadi "+soyisim+" olan kullanici sisteme giris yapti"
+# mesaj = "Adi '"+isim+"' soyadi '"+soyisim + "' olan kullanici sisteme giris yapti"
+print(mesaj)
+# print("Hosgeldin "+isim)
+
