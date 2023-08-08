ed2a1919408354f308e7047f6efc1fea844160af
Skytech <93709385+saitTheCoder@users.noreply.github.com>
2023-08-08 22:37:45 +0300

    Add files via upload

---
+++ Y Python A/3.ders.py
@@ -0,0 +1,34 @@
+# # f-string
+# name = "sait"
+# age = 38
+# # String Birlestirme
+# print(name + " is "+ str(age)+ " years old")
+# print(name,"is",age,"years old")
+# # data type - veri tipi
+# # 1- String - Text - Metinsel Ifade
+# # 2- Integer numbers 
+# # 3- Floating Point Numbers
+
+# # f-string
+# print(f"{name} is {age} years old")
+# name = "Mark"
+# age = 37
+# city = "Berlin"
+# print(f"{name} is {age} years old. {name} lives in {city}")
+# BMI = kg/m2 
+
+# name = input("Enter your name: ")
+# weight = float(input("Enter your weight: "))
+# height = float(input("Enter your height: "))
+
+# bmi = weight / height ** 2
+
+# print(f"Body Mass index of {name} is {bmi}")
+
+# kullanicidan 3 tane sayi aliniz. sonra bu sayilarin toplamini hesaplayiniz
+
+number = int(input("Enter first number: ")) # 5
+number = int(input("Enter second number: ")) # 3
+number = int(input("Enter third number: ")) # 10
+
+sum = number + number + number
